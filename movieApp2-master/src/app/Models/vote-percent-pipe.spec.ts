18fa47bae4a802b5f7b8d6b91497f8001f20cf98
nagat1 <130458763+nagat1@users.noreply.github.com>
2025-06-20 14:20:37 +0300

    Delete movieApp2-master directory

--- movieApp2-master/src/app/Models/vote-percent-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { VotePercentPipe } from './vote-percent-pipe';
-
-describe('VotePercentPipe', () => {
-  it('create an instance', () => {
-    const pipe = new VotePercentPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
